8b43997eb5a9ea9b402c2a8b3a3e1fce917cf59b
Denny Alquinta <dralquinta@google.com>
2024-09-25 19:09:40 -0300

    Randomly entering 50% chance of 500 Error

--- src/main/java/com/example/sample/microservice/demo_hello_world/DemoHelloWorldApplication.java
+++ src/main/java/com/example/sample/microservice/demo_hello_world/DemoHelloWorldApplication.java
@@ -3,31 +3,31 @@ package com.example.sample.microservice.demo_hello_world;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Random;
 
 @SpringBootApplication
 public class DemoHelloWorldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoHelloWorldApplication.class, args);
-	}
-
-@RestController
-class MockController {
-
-    @GetMapping("/")
-    public String hello() {
-        return "Hello, World!";
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHelloWorldApplication.class, args);
     }
 
-	// Redirect root (/) to /hello
-        @RequestMapping("/")
-        @ResponseBody
-        public String redirectToHello() {
-            return "redirect:/hello";
-        }
-}
+    @RestController
+    class MockController {
 
+        private final Random random = new Random();
+
+        @GetMapping("/")
+        public ResponseEntity<String> hello() {
+            // Introduce a 50% chance of returning a 500 error
+            if (random.nextInt(100) < 50) {
+                return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+            return new ResponseEntity<>("Hello, World!", HttpStatus.OK);
+        }
+    }
 }
