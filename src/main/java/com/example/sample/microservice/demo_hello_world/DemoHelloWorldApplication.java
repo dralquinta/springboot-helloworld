8e4a412941ec2666b08ce3cb009fe996a2cb74d6
Denny Alquinta <dralquinta@google.com>
2024-09-25 11:28:23 -0300

    Updating java backend

--- src/main/java/com/example/sample/microservice/demo_hello_world/DemoHelloWorldApplication.java
+++ src/main/java/com/example/sample/microservice/demo_hello_world/DemoHelloWorldApplication.java
@@ -2,6 +2,8 @@ package com.example.sample.microservice.demo_hello_world;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoHelloWorldApplication {
