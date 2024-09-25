ae64e527dde577890d0587f33698f243c4a3e503
Denny Alquinta <dralquinta@google.com>
2024-09-25 19:05:04 -0300

    Updating implementation to always redirect to /

--- src/main/java/com/example/sample/microservice/demo_hello_world/DemoHelloWorldApplication.java
+++ src/main/java/com/example/sample/microservice/demo_hello_world/DemoHelloWorldApplication.java
@@ -3,6 +3,8 @@ package com.example.sample.microservice.demo_hello_world;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -19,6 +21,13 @@ class MockController {
     public String hello() {
         return "Hello, World!";
     }
+
+	// Redirect root (/) to /hello
+        @RequestMapping("/")
+        @ResponseBody
+        public String redirectToHello() {
+            return "redirect:/hello";
+        }
 }
 
 }
