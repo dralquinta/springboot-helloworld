fdb66b8eb647501c8997091b180906b2e0ed2697
Denny Alquinta <dralquinta@google.com>
2024-09-25 11:24:14 -0300

    Updating docker image

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 mvn clean package
 
-gcloud builds submit --tag southamerica-west1-docker.pkg.dev/springboot-angular-sample/springboot-angular-sample/springboot-angular-sample
+gcloud builds submit --tag southamerica-west1-docker.pkg.dev/springboot-helloworld/springboot-helloworld/springboot-helloworld
 
-gcloud run deploy springboot-angular-sample \
-  --image southamerica-west1-docker.pkg.dev/springboot-angular-sample/springboot-angular-sample/springboot-angular-sample \
+gcloud run deploy springboot-helloworld \
+  --image southamerica-west1-docker.pkg.dev/springboot-helloworld/springboot-helloworld/springboot-helloworld \
   --platform managed \
   --region southamerica-west1 \
   --allow-unauthenticated \
