dd4eb9f81972ae991339cc98e016e891c70f1ff3
Denny Alquinta <dralquinta@google.com>
2024-09-25 15:45:50 -0300

    Updating contexts

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 mvn clean package
 
-cd target
-gcloud builds submit . --tag southamerica-west1-docker.pkg.dev/springboot-helloworld/springboot-helloworld/springboot-helloworld
+cd target/
+gcloud builds submit  --tag southamerica-west1-docker.pkg.dev/springboot-helloworld/springboot-helloworld/springboot-helloworld
 
 gcloud run deploy springboot-helloworld \
   --image southamerica-west1-docker.pkg.dev/springboot-helloworld/springboot-helloworld/springboot-helloworld \
